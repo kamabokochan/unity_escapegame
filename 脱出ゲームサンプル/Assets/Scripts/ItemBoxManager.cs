a4d54e016139a5e1c49b0fa4b7ed972c669f80c7
kamabokochan <caffe@harachan.local>
2020-11-28 02:02:42 +0900

    ignore適用

---
+++ 脱出ゲームサンプル/Assets/Scripts/ItemBoxManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ITEM {
+    NONE,
+    LIGHT_BULB
+}
+
+public class ItemBoxManager : MonoBehaviour {
+    [SerializeField] Sprite lightBulbSprite; // 電球画像
+    [SerializeField] Image[] itemBoxImages;
+    [SerializeField] LightStandManager lightStandManager;
+    [SerializeField] GameManager gameManager;
+
+    ITEM[] itemsList = new ITEM[4]; // 取得したアイテムの配列
+
+    // アイテムを取得した
+    public void SetItem(ITEM item) {
+        itemsList[0] = item;
+        switch (item) {
+            case ITEM.LIGHT_BULB:
+                itemBoxImages[0].sprite = lightBulbSprite;
+                break;
+            default:
+            case ITEM.NONE:
+                itemBoxImages[0].sprite = null;
+                break; 
+        }
+    }
+
+    // アイテムを使用
+    public void UseItem(int index) {
+        if (gameManager.currentPanel == PANEL.LIGHT_STAND && itemsList[index] == ITEM.LIGHT_BULB) {
+            lightStandManager.LightSwitch(true);
+            itemsList[index] = ITEM.NONE; // アイテムを使用したので空にする
+            itemBoxImages[index].sprite = null;
+        }
+    }
+}
---
+++ 脱出ゲームサンプル/Assets/Scripts/PCManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PCManager : MonoBehaviour {
+    [SerializeField] Text[] viewPasswordTexts;
+    [SerializeField] Text passwordPanelText;
+
+    // ユーザが入力したパスワード
+    string userInputPassword = "";
+
+    //　正解のパスワード
+    string correctPassword = "3333";
+
+    int currentIndex = 0;
+
+    bool isClear = false;
+
+    public void InputKey(int password) {
+        if (isClear) {
+            return; // クリアしていたら何も操作させない
+        }
+
+        viewPasswordTexts[currentIndex].text = password.ToString();
+        userInputPassword += password.ToString();
+        currentIndex++;
+        if (currentIndex >= viewPasswordTexts.Length) {
+            if (CheckPass()) {
+                Debug.Log("正解");
+                isClear = true;
+                passwordPanelText.text = "CLEAR";
+            } else {
+                Debug.Log("はずれ");
+                // リセットする
+                userInputPassword = "";
+                currentIndex = 0;
+            }
+        }
+    }
+
+    bool CheckPass() {
+        Debug.Log(userInputPassword);
+        return userInputPassword == correctPassword;
+    }
+}
